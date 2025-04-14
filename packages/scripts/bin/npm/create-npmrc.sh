665da4836a4d38e6c87245f89c4b7031f595da55
Rishi Mukherjee <rishitech882@gmail.com>
2025-04-15 00:22:59 +0530

    checkpoint

--- packages/scripts/bin/npm/create-npmrc.sh
+++ packages/scripts/bin/npm/create-npmrc.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 TARGET_DIR="${1:-.}"  # defaults to current directory if no argument
 
-rm -f "$TARGET_DIR/.npmrc"
 if [ -z "${GITHUB_TOKEN}" ]; then
   echo "Error: GITHUB_TOKEN missing" >&2
   exit 1
 fi
-echo "@dmsconnect:registry=https://npm.pkg.github.com\n//npm.pkg.github.com/:_authToken=${GITHUB_TOKEN}">>"$TARGET_DIR/.npmrc"
+
+echo "@dmsconnect:registry=https://npm.pkg.github.com" > "$TARGET_DIR/.npmrc"
+echo "//npm.pkg.github.com/:_authToken=${GITHUB_TOKEN}" >> "$TARGET_DIR/.npmrc"
